fcbc8ed766c649ebb31748e77f6313bf5fc5bf5a
Ekwusie Sophia <sophiaekwusie70@gmail.com>
2024-08-19 20:25:58 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CaptionFeature from './Components/CaptionFeature';
+
+function App() {
+  return (
+    <div className="App">
+      <CaptionFeature />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/CaptionFeature.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const CaptionFeature = () => {
+  const balls = Array.from({ length: 100 });
+
+  return (
+    <div className='caption-feature'>
+      {balls.map((_, index) => (
+        <div 
+          key={index} 
+          className='bouncing-ball' 
+          style={{ 
+            backgroundColor: index % 2 === 0 ? '#fff' : '#ffd700',
+            top: `${Math.random() * 100}%`,
+            left: `${Math.random() * 100}%`,
+            animationDuration: `${Math.random() * 4 + 3}s`,
+          }}
+        ></div>
+      ))}
+      <div className='capfeature-container'>
+        <h1>Image Caption Generator</h1>
+        <div className='function-buttons'>
+          <button className='upload-button'>Browse...</button>
+          <button className='generator-button'>Generate Caption</button>
+        </div>
+          <div className='display-field'>Display the Caption here ...</div>
+      </div>
+    </div>
+  )
+}
+
+export default CaptionFeature;
